b0740b747e20fc933e8cf70e5d590ddc08fb539e
jakenorris1124 <jakenorris1124@gmail.com>
2023-03-27 15:42:09 -0400

    Fixed camera moving when paused

--- Assets/Scenes/MenuUI/Pause.cs
+++ Assets/Scenes/MenuUI/Pause.cs
@@ -8,12 +8,16 @@ public class Pause : MonoBehaviour
 {
     public GameObject PauseMenu;
     public GameObject Player;
+    public Camera MainCam;
+    
     public static bool isPaused;
+    private CinemachineBrain camBrain;
 
     // Start is called before the first frame update
     void Start()
     {
         PauseMenu.SetActive(false);
+        camBrain = MainCam.GetComponent<CinemachineBrain>();
     }
 
     // Update is called once per frame
@@ -39,6 +43,7 @@ public class Pause : MonoBehaviour
         isPaused = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        camBrain.m_UpdateMethod = CinemachineBrain.UpdateMethod.FixedUpdate;
         
         Player.GetComponent<PlayerMovement>().enabled = false;
     }
@@ -49,7 +54,8 @@ public class Pause : MonoBehaviour
         Time.timeScale = 1f;
         isPaused = false;
         Cursor.visible = false;
-        
+        camBrain.m_UpdateMethod = CinemachineBrain.UpdateMethod.SmartUpdate;
+
         Player.GetComponent<PlayerMovement>().enabled = true;
     }
 
