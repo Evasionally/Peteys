efee6d1b89c6b8c237c94bad608e763d72112f87
Evasionally <91702636+Evasionally@users.noreply.github.com>
2023-03-26 22:35:06 -0400

    Level 1 updates put into menu

---
+++ Assets/Scenes/Level 1/Scripts/MazeTileWaypoints.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeTileWaypoints : MonoBehaviour
+{
+    public Transform GetWaypoint(int waypointIndex)
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    public int GetNextWaypointIndex(int currentWaypointIndex)
+    {
+        int nextWaypointIndex = Random.Range(0, transform.childCount);
+        return nextWaypointIndex;
+    }
+}
