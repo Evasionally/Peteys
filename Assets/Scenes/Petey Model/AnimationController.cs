612d0c4a54eeb5fc6b2f7444714f51ace464b487
Evasionally <91702636+Evasionally@users.noreply.github.com>
2023-04-03 15:56:09 -0400

    Bash Animation

--- Assets/Scenes/Petey Model/AnimationController.cs
+++ Assets/Scenes/Petey Model/AnimationController.cs
@@ -17,13 +17,29 @@ public class AnimationController : MonoBehaviour
     {
         if (ptAnimator != null)
         {
-            if (Input.GetKeyDown("w"))
+            // if running forward, left, or right
+            if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("d"))
             {
                  ptAnimator.SetBool("IsRunning", true);
             }
-            else if (Input.GetKeyUp("w"))
+            else if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("d"))
             {
                 ptAnimator.SetBool("IsRunning", false);
+            }
+
+            if (Input.GetKeyDown("s"))
+            {
+                ptAnimator.SetBool("IsBacking", true);
+
+            }
+            else if (Input.GetKeyUp("s"))
+            {
+                ptAnimator.SetBool("IsBacking", false);
+            }        
+
+            if (Input.GetKeyDown("q"))
+            {
+                ptAnimator.SetTrigger("Bash");
             }
         }
     }
