20ebec4f7e1f2e143a478cb351636b063b4f2f82
Evasionally <91702636+Evasionally@users.noreply.github.com>
2023-04-04 23:30:29 -0400

    Finished all animations

--- Assets/Scenes/Petey Model/AnimationController.cs
+++ Assets/Scenes/Petey Model/AnimationController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     private Animator ptAnimator;
+    bool IsGrounded;
+    bool Grappling; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,18 +17,25 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        IsGrounded = GameObject.Find("Petey").GetComponent<PlayerMovement>().grounded;
+        Grappling = GameObject.Find("Petey").GetComponent<Grapple>().going;
+        ptAnimator.SetBool("Grounded", IsGrounded);
+        ptAnimator.SetBool("IsSwinging", Grappling);
+
         if (ptAnimator != null)
         {
-            // if running forward, left, or right
-            if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+
+            // if running forward, left, or right play forward run
+            if ((Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("d")))
             {
                  ptAnimator.SetBool("IsRunning", true);
             }
-            else if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+            else if ((Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("d")))
             {
                 ptAnimator.SetBool("IsRunning", false);
             }
 
+            // if running back, play back run
             if (Input.GetKeyDown("s"))
             {
                 ptAnimator.SetBool("IsBacking", true);
@@ -37,10 +46,27 @@ public class AnimationController : MonoBehaviour
                 ptAnimator.SetBool("IsBacking", false);
             }        
 
+            // If bashing play bash animation
             if (Input.GetKeyDown("q"))
             {
                 ptAnimator.SetTrigger("Bash");
             }
+
+            // If jumping play jump
+            if (Input.GetKeyDown("space"))
+            {
+                ptAnimator.SetTrigger("Jump");
+            } 
+
+            // if (IsSwinging == true)
+            // {
+            //     ptAnimator.SetTrigger("StartSwing");
+            //     ptAnimator.SetBool("IsSwinging", true);
+            // }
+            // else if (IsSwinging == false)
+            // {
+            //     ptAnimator.SetBool("IsSwinging", false);
+            // }            
         }
     }
 }
