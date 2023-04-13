404f92bdcd00c0b0c79b314b5be0b53410097c30
Gian-Alvarez <106183872+Gian-Alvarez@users.noreply.github.com>
2023-04-13 17:24:05 -0400

    Fixed Level 1 Jumps
    - Fixed all Level 1 Jumps.
    - Added Level Selection to Main Menu.

--- Assets/Scenes/Levels/Level 0 - Title Screen/Scripts/MainMenu.cs
+++ Assets/Scenes/Levels/Level 0 - Title Screen/Scripts/MainMenu.cs
@@ -20,4 +20,19 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
 }
