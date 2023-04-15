59b96ed725627651cdd6456a16d73f2ebb8eb668
Gian-Alvarez <106183872+Gian-Alvarez@users.noreply.github.com>
2023-04-14 22:30:15 -0400

    Enemy Respawn System

---
+++ Assets/EnemyRespawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRespawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rangedEnemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
