ffe457ee688cb3c713ee196c035564802838c8cc
Jake Norris <jakenorris1124@gmail.com>
2023-04-25 02:50:13 -0400

    Improved movement when camera is looking down
    I wasn't able to fix the issue entirely, but it feels better

--- Assets/Scenes/Other Files/Entities/Petey/Move & Aim/PlayerMovement.cs
+++ Assets/Scenes/Other Files/Entities/Petey/Move & Aim/PlayerMovement.cs
@@ -147,8 +147,21 @@ public class PlayerMovement : MonoBehaviour {
         if (grounded && crouching) multiplierV = 0f;
 
         //Apply forces to move player
-        rb.AddForce(playerCam.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
-        rb.AddForce(playerCam.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+        Vector3 forward = new Vector3(playerCam.transform.forward.x, 0, playerCam.transform.forward.z);
+        Vector3 right = new Vector3(playerCam.transform.right.x, 0, playerCam.transform.right.z);
+        
+        Debug.DrawRay(playerCam.transform.position, forward, Color.blue, Time.deltaTime);
+        Debug.DrawRay(playerCam.transform.position, right, Color.red, Time.deltaTime);
+
+        Vector3 forwardForce = forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV;
+        Vector3 rightForce = right * x * moveSpeed * Time.deltaTime * multiplier;
+        
+        Debug.DrawRay(gameObject.transform.position, forwardForce, Color.blue, Time.deltaTime);
+        Debug.DrawRay(gameObject.transform.position, rightForce, Color.red, Time.deltaTime);
+        rb.AddForce(forwardForce);
+        rb.AddForce(rightForce);
+        
+        Debug.DrawRay(gameObject.transform.position, rb.velocity, Color.green, Time.deltaTime);
     }
 
     private void Jump() {
